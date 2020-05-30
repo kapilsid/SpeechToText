0afa04560a46cd4e78099d42bbbec9915cf6fd7f
kapil <kapilsiddharth72@gmail.com>
2020-05-30 20:12:30 +0530

    addads

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
 from app import app
 
-config = {
-    "development": "config.Development"
-}
 
 if __name__ == '__main__':
-    app.config.from_object(config["development"])
     app.run(host='0.0.0.0',port=8090)
